2ef65ec35ceea4da8143009204e3a865821692fd
Xue Chen <xueqingc@gmail.com>
2018-05-17 23:36:19 -0700

    pass test for pluckFirstLineFromFile i	modified:   src/components/search.jsx

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,35 @@
 
 var fs = require('fs');
 var request = require('request');
+var https = require('https');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data)=>{
+    if(err){
+      callback(err);
+    } else {
+      var newData = data.toString().split('\n');
+      console.log(newData[0]);
+      callback(err, newData[0]);
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  https.get(url, (res)=>{
+    if(res.statusCode === 200){
+      callback(null, res.statusCode);
+    } 
+    if (res.statusCode === 404){
+      var err = {message: 'Invalid URI'};
+      callback(err, res.statuCode);
+    }
+  });
+   
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,6 +11,15 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise((resolve, reject)=>{
+    request.get(filePath, (err, resp, body)=>{
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
